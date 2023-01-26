22acc3b4bbb5c51d9741aff8f03b89041ac8e410
Mukesh <mukeshchugh5@gmail.com>
2023-01-26 08:55:58 +0530

    login flow with authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,50 @@
 import React, {useState} from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../constants';
 
 export const Login = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: ""
+        }
+    }
 
-    const handleRegister = (e) => {
+    const handleLogin = (e) => {
+        if (email === ''){
+            alert('Email is required')
+        }
+        else if (password === ''){
+            alert('Password is required')
+        }
+        else{
+            const payload = {
+                email: email,
+                password: password
+            }
+            axios.post(`${BASE_URL}/login`, payload, config).then(res =>{
+                alert('You are successfully logged in')
+                localStorage.setItem('token', res.data?.token)
+                props.setAuthentication(true)
+            }).catch(err => {
+                console.log(err)
+                const errMessage = err.request.response
+                alert(errMessage)
+            })
+        }
         e.preventDefault();
-        console.log(email)
     }
 
     return (
         <div className='submit-form-container'>
             <h2>Login</h2>
-            <form className='form-data'>
+            <form className='form-data' onSubmit={handleLogin}>
                 <label htmlFor="email">email</label>
-                <input value={email} type="email" placeholder="your@email.com" name="email"/>
+                <input value={email} type="email" placeholder="your@email.com" name="email" onChange={(e) => setEmail(e.target.value)}/>
                 <label htmlFor="password">password</label>
-                <input value={password} type="password" placeholder="*****" name="password"/>
+                <input value={password} type="password" placeholder="*****" name="password" onChange={(e) => setPassword(e.target.value)}/>
                 <button>LogIn</button>
             </form>
             <button onClick={() => props.togglePage('register')}>New Here? Register Now</button>
