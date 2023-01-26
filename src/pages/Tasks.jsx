1ed1e7a60658cf3e48a60bb9b3565543056afed4
Mukesh <mukeshchugh5@gmail.com>
2023-01-26 13:41:34 +0530

    some css and app changes

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
+import { AddTask } from "./AddTask"
+import { Header } from "./Header"
+import { Logout } from "./Logout"
+import { TaskList } from "./TaskList"
 export const Tasks = (props) => {
+    const [updateList, setUpdateList] = useState(false)
+    const [editTask, setEditTask] = useState(null)
+    const [editTaskFlag, setEditTaskFlag] = useState(false)
+
+    const updateTaskList = (value) => {
+        setUpdateList(value)
+    }
+
+    const handleEditTask = (task) => {
+        setEditTask(task)
+        setEditTaskFlag(!editTaskFlag)
+    }
+
     return (
         <>
-            All your Tasks will be shown here
+        <div>
+            <Header/>
+        </div>
+        <div>
+            <AddTask updateTaskList={updateTaskList} updateList={updateList} editTask={editTask} editTaskFlag={editTaskFlag} handleEditTask={handleEditTask}/>
+        </div>
+        <div>
+            <TaskList updateTaskList={updateTaskList} updateList={updateList} handleEditTask={handleEditTask}/>
+        </div>
+        <div>
+            <Logout logout={props.logout}></Logout>
+        </div>
         </>
     )
 }
